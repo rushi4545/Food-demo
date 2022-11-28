c88e6548ff48985013564033b59d1e454a1f156d
rushi1006 <rushikeshpisole3@gmail.com>
2022-11-28 18:42:52 +0530

    original file added

---
+++ index.php
@@ -0,0 +1,35 @@
+<!--Admin panel-->
+
+<html>
+    <head>
+        <title>Food Order Wesite - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <div class='menu text-center'>
+            <div class ="wrapper">
+             <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Admin Manager</a></li>
+                 <li><a href="#">Catagory</a></li>
+                 <li><a href="#">Food</a></li>
+                 <li><a href="#">Order</a></li>
+                 </ul>
+                </div>
+        </div>
+
+        <div class= "main-content">
+            <div class = "wrapper">
+            <strong>DASHBOARD</strong>
+            </div>
+        </div>
+
+        <div class ="footer">
+            <div class = "wrapper">
+                <p class="text-center">2022 All right reserved, some Restaurent. Developed By - <a href="#">Sip n Snacks</a></p>
+            </div>  
+        </div>  
+    </body>
+</html>
