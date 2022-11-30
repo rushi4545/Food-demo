070f0eb16204fda6ce640a52af0ed03cac0ddfc3
rushi1006 <rushikeshpisole3@gmail.com>
2022-11-30 19:59:15 +0530

    changes

--- admin/admin.php
+++ admin/admin.php
@@ -1,19 +1,23 @@
-<!--Admin panel-->
+<!--Admin Panel-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="admin.css">
-    <title>Food Order Wesite - Home Page</title>
+    <title>Sip n snacks</title>
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 <body>
     
 </body>
 </html>
+
+</body>
+</html>
     <body>
-        <div class='menu text-center'>
+        <div class="menu text-center">
             <div class ="wrapper">
              <ul>
                  <li><a href="#">Home</a></li>
@@ -24,51 +28,49 @@
                  </ul>
                 </div>
         </div>
-
-        <div class= "menu-content">
-            <div class = "wrapper">
-            <h1>Manage Admin</h1>
-               
-
-                <div class= "clearfix"></div>
-
-             </div>
         </div>
 
+  <div class= "menu-content">
+    <div class = "wrapper">
+    <h1>Dashboard</h1>
        
-        <div class ="footer">
-            <div class = "wrapper">
-                <p class="text-center"> 2022 All right reserved, some Restaurent. Developed By- <a href="#">Sip n Snacks</a></p>
-            </div>  
-        </div>  
-    </body>
-</html>
 
+    <div class= "col-4"></div>
+        <div>5</div>
+        <br />
+        categories
+        </div>
+    </div>
 
+    <div class= "col-4"></div>
+        <div>5</div>
+        <br />
+        categories
+        </div>
+    </div>
 
+    <div class= "col-4"></div>
+        <div>5</div>
+        <br />
+        categories
+        </div>
+    </div>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documezxvsd</title>
-</head>
-<body>
+    <div class= "col-4"></div>
+        <div>5</div>
+        <br />
+        categories
+        </div>
+    </div>
     
-</body>
-</html>
+    <div class ="clearfix"></div>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documzx,v jsent</title>
-</head>
-<body>
-    
+</div>
+<div class ="footer">
+    <div class = "wrapper">
+        <p class="text-center"> Developed By- <a href="#">Sip n Snacks</a></p>
+    </div>  
+</div>  
+
 </body>
 </html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,55 @@
+<!--Admin Panel-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sip n snacks</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+</body>
+</html>
+    <body>
+        <div class="menu text-center">
+            <div class ="wrapper">
+             <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">Admin</a></li>
+                 <li><a href="#">Catagory</a></li>
+                 <li><a href="#">Food</a></li>
+                 <li><a href="#">Order</a></li>
+                 </ul>
+                </div>
+        </div>
+        </div>
+
+  <div class= "menu-content">
+    <div class = "wrapper">
+    <h1>Manage Admin</h1>
+       
+
+    <div class= "col-4"></div>
+        <div>5</div>
+        <br />
+        categories
+        </div>
+    </div>
+    
+    <div class ="clearfix"></div>
+
+</div>
+<div class ="footer">
+    <div class = "wrapper">
+        <p class="text-center"> Developed By- <a href="#">Sip n Snacks</a></p>
+    </div>  
+</div>  
+
+</body>
+</html>
