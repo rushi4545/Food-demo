dd3258352c50188d18319bb25ccdab4f0b338a0f
rushi1006 <rushikeshpisole3@gmail.com>
2022-11-30 11:13:54 +0530

    changes

--- admin/admin.php
+++ admin/admin.php
@@ -1,12 +1,17 @@
 <!--Admin panel-->
-
-<html>
-    <head>
-        <title>Food Order Wesite - Home Page</title>
-
-        <link rel="stylesheet" href="css\admin.css">
-    </head>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css">
+    <title>Food Order Wesite - Home Page</title>
+</head>
+<body>
+    
+</body>
+</html>
     <body>
         <div class='menu text-center'>
             <div class ="wrapper">
